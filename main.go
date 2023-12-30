635c39cca55c31a5e8127e3ead62ab8d6fe19c29
Yuri <yuri.anderson.lobo@gamail.com>
2023-12-30 15:22:56 -0300

    funcao sacar

--- main.go
+++ main.go
@@ -9,20 +9,22 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
 func main() {
 
 	contaDoYuri := ContaCorrente{"Yuri", 323, 11222, 311}
 
-	contaDoGuilherme := ContaCorrente{titular: "Guilherme",
-		numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
+	fmt.Println(contaDoYuri.saldo)
 
-	fmt.Println(contaDoYuri)
-	fmt.Println(contaDoGuilherme)
+	fmt.Println(contaDoYuri.Sacar(5))
+	fmt.Println(contaDoYuri.saldo)
 
-	var contaDaCris *ContaCorrente
-	contaDaCris = new(ContaCorrente)
-	contaDaCris.titular = "Cris"
-	contaDaCris.saldo = 500
-
-	fmt.Println(contaDaCris)
 }
