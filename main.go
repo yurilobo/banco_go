15a3c47b0b31be280e1e606e69a8db82fcc0d4a1
Yuri <yuri.anderson.lobo@gamail.com>
2023-12-30 09:47:54 -0300

    Variaveis e tipos

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var titular string = "Guilherme"
+	var numeroAgencia int = 589
+	var numeroConta int = 12346
+	var saldo float64 = 125.50
+
+	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
+
+	var titular2 string = "Luciane"
+	var numeroAgencia2 int = 579
+	var numeroConta2 int = 126
+	var saldo2 float64 = 15796.50
+	fmt.Println(titular2, numeroAgencia2, numeroConta2, saldo2)
+}
