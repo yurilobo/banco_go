c2894caba8e6de065de5a01ba06bf635dfdd3737
Yuri <yuri.anderson.lobo@gamail.com>
2024-01-02 19:38:01 -0300

    alterando visibilidade

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"banco_oo/clientes"
 	"banco_oo/contas"
 	"fmt"
 )
 
 func main() {
-	clienteBruno := clientes.Titular{"Yuri", "123.254.426.85", "Desenvolvedor"}
-	contaDoBruno := contas.ContaCorrente{clienteBruno, 123, 12645, 1000}
-	fmt.Println(contaDoBruno)
+	contaExemplo := contas.ContaCorrente{}
+	contaExemplo.Depositar(100)
 
+	fmt.Println(contaExemplo.ObterSaldo())
 }
