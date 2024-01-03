82869d39ccca47b00ef47e71a077234cebd71c48
Yuri <yuri.anderson.lobo@gamail.com>
2024-01-02 22:03:34 -0300

    interface

--- main.go
+++ main.go
@@ -5,10 +5,23 @@ import (
 	"fmt"
 )
 
+func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
 	contaDoDenis := contas.ContaPoupanca{}
 	contaDoDenis.Depositar(100)
-	contaDoDenis.Sacar(55)
+	PagarBoleto(&contaDoDenis, 60)
 
 	fmt.Println(contaDoDenis.ObterSaldo())
+	contaDaLuisa := contas.ContaCorrente{}
+	contaDaLuisa.Depositar(500)
+	PagarBoleto(&contaDaLuisa, 400)
+
+	fmt.Println(contaDaLuisa.ObterSaldo())
 }
